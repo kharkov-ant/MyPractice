53868903d3cc9955c21f5d801048084cdda9fcf6
kharkov-ant <serwak96@gmail.com>
2018-05-29 15:45:59 +0300

    add part5 in Demo

--- Practice3/src/ua/nure/kharkov/practice3/Demo.java
+++ Practice3/src/ua/nure/kharkov/practice3/Demo.java
@@ -6,6 +6,7 @@ import ua.nure.kharkov.practice3.part1.Part1;
 import ua.nure.kharkov.practice3.part2.Part2;
 import ua.nure.kharkov.practice3.part3.Part3;
 import ua.nure.kharkov.practice3.part4.Part4;
+import ua.nure.kharkov.practice3.part5.Part5;
 
 public class Demo {
     public static void main(String[] args) throws NoSuchAlgorithmException {
@@ -13,5 +14,6 @@ public class Demo {
         Part2.main(new String[] {});
         Part3.main(new String[] {});
         Part4.main(new String[] {});
+        Part5.main(new String[] {});
     }
 }
